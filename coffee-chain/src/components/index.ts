2f5e103e49b97000c71800a63293f3ba5b705e3c
kairoslab <jproa96@gmail.com>
2026-01-10 10:26:23 -0500

    Please

--- coffee-chain/src/components/index.ts
+++
@@ -1,6 +0,0 @@
-// Component exports
-
-export { Timeline } from './Timeline';
-export { FreshnessIndicator } from './FreshnessIndicator';
-export { PriceBreakdown } from './PriceBreakdown';
-export { LotCard } from './LotCard';
